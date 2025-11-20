7254156d5212d26186c88c5d6911844d066f8904
Diego Pasaye <pasayealvarado@gmail.com>
2025-11-19 22:14:23 -0600

    cambios para la mejora del SEO

--- src/app/services/personajes.ts
+++ src/app/services/personajes.ts
@@ -1,33 +1,41 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, inject, EnvironmentInjector, runInInjectionContext } from '@angular/core'; // Importamos EnvironmentInjector y runInInjectionContext
 import { HttpClient } from '@angular/common/http';
 import { RespuestaBD, RespuestaDetalle } from '../interfaces/interfaces';
-import {Firestore, collection, collectionData, doc, docData} from '@angular/fire/firestore'
+import {
+  Firestore,
+  collection,
+  collectionData,
+  doc,
+  docData
+} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Personajes {
-  constructor(private http:HttpClient, private firestore:Firestore){}
+  private injector = inject(EnvironmentInjector);
 
-  getDatos(){
-    return this.http.get<RespuestaBD>('https://practicaangular-bc58e-default-rtdb.firebaseio.com/.json')
-  }
-  // getDatos(){
-  //   return this.http.get<RespuestaBD>('https://reqres.in/api/users', {headers:{'x-api-key':'reqres-free-v1'}})
-  // }
-  getDatosById(id: number){
-    return this.http.get<RespuestaDetalle>(`https://practicaangular-bc58e-default-rtdb.firebaseio.com/.json/${id}`)
+  constructor(private http: HttpClient, private firestore: Firestore) {}
+
+  getDatos() {
+    return this.http.get<RespuestaBD>('https://practicaangular-bc58e-default-rtdb.firebaseio.com/.json');
   }
 
-  getPersonajes(){
-    const planetasCollection = collection(this.firestore, 'planetas');
-    return collectionData(planetasCollection, {
-      idField: 'id',
+  getDatosById(id: number) {
+    return this.http.get<RespuestaDetalle>(`https://practicaangular-bc58e-default-rtdb.firebaseio.com/.json/${id}`);
+  }
+
+  getPersonajes() {
+    return runInInjectionContext(this.injector, () => {
+      const personajesRef = collection(this.firestore, 'personajes');
+      return collectionData(personajesRef, { idField: 'id' });
     });
   }
-  getPersonajesDetalle(id:string){
-    const planetaDoc = doc(this.firestore, `planetas/${id}`);
-    return docData(planetaDoc, { idField: 'id' });
+
+  getPersonajesDetalle(id: string) {
+    return runInInjectionContext(this.injector, () => {
+      const personajesRef = doc(this.firestore, 'personajes', id);
+      return docData(personajesRef, { idField: 'id' });
+    });
   }
 }
